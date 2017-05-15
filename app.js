a4b2bdc0adb61bc3863a6cd00158fdcafe7a0b08
Gabriel Pereira Ramos <juniorcastrov@gmail.com>
2017-05-15 16:20:16 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/app'));
+
+app.listen(9000, () => {
+  console.log('Server runing in port 9000');
+});
