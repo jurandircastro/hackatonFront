e7583c1a9a1ea9e61866abe280d1360ae9b9835c
Ricardo Robson <rcrdrobson@gmail.com>
2017-05-21 00:05:22 -0300

    ...

--- app/services/home.service.js
+++ app/services/home.service.js
@@ -6,14 +6,6 @@ angular.module('hackaton')
 function HomeService($http, $q, httpRequest) {
     const factory = {
         createObjectClass: (data) => {
-<<<<<<< HEAD
-            const url = '/createObjectClass/' + "5";
-            const method = 'POST';
-            return httpRequest(url, method, data, $q, $http);
-        },
-          allMyObjectClass: (data) => {
-            const url = '/allMyObjectClass/' + "5";
-=======
             const url = '/createObjectClass/' + "2";
             const method = 'POST';
             return httpRequest(url, method, data, $q, $http);
@@ -25,7 +17,6 @@ function HomeService($http, $q, httpRequest) {
         },
         allMyObjectClass: (data) => {
             const url = '/allMyObjectClass/' + "1";
->>>>>>> 38192bddff124fb03691d6d034f10fb3fd803dcd
             const method = 'GET';
             return httpRequest(url, method, data, $q, $http);
         },
