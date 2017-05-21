e9889c36033b62abd00bef3ddb150f03660202ba
fabioxd20 <fabioxd.20@gmail.com>
2017-05-20 22:58:50 -0300

    alteracao porta

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/app'));
 
-app.listen(process.env.PORT || 8080, () => {
+app.listen(process.env.PORT, () => {
   console.log('Server runing in port 8080');
 });
