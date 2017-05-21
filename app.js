f2162fdacf22ce34b12606be760fb01b25b2fcdd
fabioxd20 <fabioxd.20@gmail.com>
2017-05-20 23:03:45 -0300

    alterao rota

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/app'));
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log('Server runing in port 8080');
 });
