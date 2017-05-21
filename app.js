79700b63371e7d7105b74b54261786d22c4f5194
fabioxd20 <fabioxd.20@gmail.com>
2017-05-21 10:10:05 -0300

    ajuste app.js

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const Twitter = require('twitter');
-
-
-
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    //console.log(tweets);
-  }
-});
-
-client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
-   //console.log(tweets);
-});
 
 app.use(express.static(__dirname + '/app'));
 
