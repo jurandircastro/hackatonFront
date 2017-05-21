5cb3238f3040b9e5d173a4e7fe73b56d0a6aa32f
fabioxd20 <fabioxd.20@gmail.com>
2017-05-21 08:09:27 -0300

    ajustes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var client = new Twitter({
 var params = {screen_name: 'nodejs'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
+    //console.log(tweets);
   }
 });
 
